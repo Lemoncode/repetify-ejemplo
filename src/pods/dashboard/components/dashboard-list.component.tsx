360c02dba189ae3f18531adb9806fd76c7bb8aa7
Antonio Contreras <antoniocontrerasroman@gmail.com>
2025-04-22 13:51:35 +0200

    remove import

--- src/pods/dashboard/components/dashboard-list.component.tsx
+++ src/pods/dashboard/components/dashboard-list.component.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
 import { Deck } from '../dashboard.vm';
 import { DashboardCardComponent } from './dashboard.card.component'
 import * as classes from './dashboard-list.component.style';
