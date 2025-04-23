66fcecf1e4801f0b6a21c7376b7c71658feac7f5
Antonio Contreras <antoniocontrerasroman@gmail.com>
2025-04-23 10:23:59 +0200

    create card component

--- src/pods/card/card.pod.tsx
+++ src/pods/card/card.pod.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useParams } from '@tanstack/react-router';
+import { CardComponent } from './card-component';
 
 export const DeckPod: React.FC = () => {
   const { deckId } = useParams({ strict: false });
 
-  return <h1>Card component {deckId}</h1>
+  return (
+    <CardComponent />
+  )
 }
