9369bb191cf2f41427a8e9daf77e1d14be33e4f7
Antonio Contreras <antoniocontrerasroman@gmail.com>
2025-04-23 10:29:03 +0200

    rename the component

--- src/pods/card/card.pod.tsx
+++ src/pods/card/card.pod.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from '@tanstack/react-router';
 
-export const DeckPod: React.FC = () => {
+export const CardPod: React.FC = () => {
   const { deckId } = useParams({ strict: false });
 
   return <h1>Card component {deckId}</h1>
