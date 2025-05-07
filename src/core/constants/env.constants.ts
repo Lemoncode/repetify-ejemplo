8340894184b484357e30f3ab63cdf0426d868c6f
Iván Ruiz <ivr.644@gmail.com>
2025-05-07 13:14:00 +0200

    Removed env varibles since not necessary

--- src/core/constants/env.constants.ts
+++ src/core/constants/env.constants.ts
@@ -1,5 +1,3 @@
 export const ENV = {
   IS_PRODUCTION: import.meta.env.PROD,
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
-  MICROSOFT_LOGIN_URL: import.meta.env.VITE_MICROSOFT_LOGIN_URL,
 };
--- src/pods/login/login.pod.tsx
+++ src/pods/login/login.pod.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { LoginComponent } from './login.component';
-import { ENV } from '#core/constants/env.constants.ts';
 
 export const LoginPod: React.FC = () => {
   const handleMicrosoftLogin = () => {
-    window.location.href = ENV.MICROSOFT_LOGIN_URL;
+    window.location.href = '/api/ExternalLogin/initiateMicrosoftSignin';
   };
 
   return <LoginComponent onMicrosoftLogin={handleMicrosoftLogin} />;
