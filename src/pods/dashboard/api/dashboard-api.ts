7391965af6b9ff9696135f861d3f6371aabec7c0
Iván Ruiz <ivr.644@gmail.com>
2025-05-07 12:16:57 +0200

    Added withCredentials: true and switched to promise chaining with then/catch

--- src/pods/dashboard/api/dashboard-api.ts
+++ src/pods/dashboard/api/dashboard-api.ts
@@ -2,13 +2,14 @@ import axios from 'axios';
 import { Deck } from './dashboard-api.model';
 import { ENV } from '#core/constants/env.constants.ts';
 
-export const getDecks = async (): Promise<Deck[]> => {
-  try {
-    const response = await axios.get(`${ENV.API_BASE_URL}/Deck/user-decks`);
-    const decks: Deck[] = response.data;
-    return decks;
-  } catch (error) {
-    console.error('Error fetching decks:', error);
-    return [];
-  }
+export const getDecks = (): Promise<Deck[]> => {
+  return axios
+    .get(`${ENV.API_BASE_URL}/Deck/user-decks`, {
+      withCredentials: true,
+    })
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Error fetching decks:', error);
+      return [];
+    });
 };
