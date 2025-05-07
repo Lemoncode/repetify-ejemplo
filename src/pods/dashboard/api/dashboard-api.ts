0f97eb165d1cff61f7c1763f48f4c8007b6e9cbe
Iván Ruiz <ivr.644@gmail.com>
2025-05-07 09:54:03 +0200

    Added request to the api to obtain the decks

--- src/pods/dashboard/api/dashboard-api.ts
+++ src/pods/dashboard/api/dashboard-api.ts
@@ -1,6 +1,14 @@
+import axios from 'axios';
 import { Deck } from './dashboard-api.model';
-import { decks } from './dashboard.mock-data';
+import { ENV } from '#core/constants/env.constants.ts';
 
 export const getDecks = async (): Promise<Deck[]> => {
-  return Promise.resolve(decks);
-}
+  try {
+    const response = await axios.get(`${ENV.API_BASE_URL}/Deck/user-decks`);
+    const decks: Deck[] = response.data;
+    return decks;
+  } catch (error) {
+    console.error('Error fetching decks:', error);
+    return [];
+  }
+};
