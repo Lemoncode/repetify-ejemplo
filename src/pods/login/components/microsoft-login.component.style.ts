b8849bca6fca4fc1cdf60cd56235c92336d60939
Iván Ruiz <ivr.644@gmail.com>
2025-05-08 09:56:23 +0200

    Converted microsoft login button to MUI

--- src/pods/login/components/microsoft-login.component.style.ts
+++ src/pods/login/components/microsoft-login.component.style.ts
@@ -1,31 +1,32 @@
-import { css } from '@emotion/css';
+import { SxProps, Theme } from '@mui/material/styles';
+import { theme } from '#core/theme/theme.ts';
 
-export const microsoftButton = css`
-  position: relative;
-  background-color: hsl(0, 0%, 95.3%);
-  border: none;
-  border-radius: 50%;
-  width: 3rem;
-  height: 3rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
+export const microsoftButton: SxProps<Theme> = {
+  position: 'relative',
+  backgroundColor: theme.palette.grey[200],
+  border: 'none',
+  borderRadius: '50%',
+  width: '3rem',
+  aspectRatio: '1/1',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '1rem',
+  cursor: 'pointer',
+  transition: 'all 0.2s ease-in-out',
 
-  &:hover {
-    background-color: rgb(71, 71, 71);
-    transform: scale(1.1);
-  }
+  '&:hover': {
+    backgroundColor: theme.palette.grey[700],
+    transform: 'scale(1.1)',
+  },
 
-  img {
-    width: 24px;
-    position: absolute;
-    transition: filter 0.2s;
-  }
+  '& img': {
+    width: '24px',
+    position: 'absolute',
+    transition: 'filter 0.2s',
+  },
 
-  &:hover img {
-    filter: brightness(0) invert(1);
-  }
-`;
+  '&:hover img': {
+    filter: 'brightness(0) invert(1)',
+  },
+};
--- src/pods/login/components/microsoft-login.component.tsx
+++ src/pods/login/components/microsoft-login.component.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import * as classes from './microsoft-login.component.style';
-
+import { Button } from '@mui/material';
 interface Props {
   onMicrosoftLogin: () => void;
 }
 
 export const MicrosoftLoginComponent: React.FC<Props> = ({ onMicrosoftLogin }) => {
   return (
-    <button className={classes.microsoftButton} onClick={onMicrosoftLogin}>
+    <Button sx={classes.microsoftButton} onClick={onMicrosoftLogin}>
       <img src="/microsoft-logo.svg" alt="Logo Microsoft" />
-    </button>
+    </Button>
   );
 };
