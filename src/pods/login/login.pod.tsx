e4b7bacc983a403d715feec1e27f55f2db8ea929
manudous <manudous@gmail.com>
2025-05-13 17:52:10 +0200

    Add returnUrl parameter to Microsoft login redirect for proper navigation

--- src/pods/login/login.pod.tsx
+++ src/pods/login/login.pod.tsx
@@ -3,7 +3,7 @@ import { LoginComponent } from './login.component';
 
 export const LoginPod: React.FC = () => {
   const handleMicrosoftLogin = () => {
-    window.location.href = '/api/ExternalLogin/initiateMicrosoftSignin';
+    window.location.href = '/api/ExternalLogin/initiateMicrosoftSignin?returnUrl=http://localhost:8080/dashboard';
   };
 
   return <LoginComponent onMicrosoftLogin={handleMicrosoftLogin} />;
